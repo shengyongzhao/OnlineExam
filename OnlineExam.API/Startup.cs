1aafc19e78fe1a9219c82d246c7dcfc35943a1ef
ceaser.zhao <shengyongzhao@hotmail.com>
2019-01-21 13:50:45 +0800

    add swagger

--- OnlineExam.API/Startup.cs
+++ OnlineExam.API/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace OnlineExam.API
 {
@@ -26,6 +27,11 @@ namespace OnlineExam.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "ConlineExam API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +47,13 @@ namespace OnlineExam.API
             }
 
             app.UseHttpsRedirection();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "OnlineExam V1");
+            });
+
             app.UseMvc();
         }
     }
